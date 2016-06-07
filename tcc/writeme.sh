9c8c1e839622287ce3f700166b4d8eff2e2dd930
Gustavo Tsuji <gustavokt@gmail.com>
2016-06-06 21:20:34 -0300

    Create thesis structure

---
+++ tcc/writeme.sh
@@ -0,0 +1,5 @@
+rm *.aux *.lo* 
+pdflatex abntex2-modelo-trabalho-academico
+bibtex abntex2-modelo-trabalho-academico
+pdflatex abntex2-modelo-trabalho-academico
+pdflatex abntex2-modelo-trabalho-academico
